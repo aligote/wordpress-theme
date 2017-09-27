708f294759ae86614848e7562f4a058d997bd186
Arfan Mahmood <arfan.mahmood@hotmail.com>
2017-09-27 17:37:20 +0500

    Update 0.0.1
    # header.php updates
    - Dynamic title picking
    - Meta author tag updates
    - Filter extra meta tags
    - Dynamic path for favicon.ico
    - Removed integrity and cross origin attributes from bootstrap css tag
    
    # style.css update
    - Updated default theme name.

--- wp/wp-content/themes/demo/header.php
+++ wp/wp-content/themes/demo/header.php
@@ -10,28 +10,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <!-- Important stuff for SEO, don't neglect. (And don't dupicate values across your site!) -->
-    <title></title>
-    <meta name="author" content="" />
+    <title><?php wp_title('&laquo;', true, 'right'); ?></title>
+    <meta name="author" content="Bits Technology" />
 
     <!-- Don't forget to set your site up: http://google.com/webmasters -->
     <meta name="google-site-verification" content="" />
 
     <!-- Who owns the content of this site? -->
-    <meta name="Copyright" content="" />
+    <meta name="Copyright" content="<?php bloginfo('name'); ?>" />
 
-    <!--  Mobile Viewport
-    http://j.mp/mobileviewport & http://davidbcalhoun.com/2010/viewport-metatag
-    device-width : Occupy full width of the screen in its current orientation
-    initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
-    maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width (wrong for most sites)
-    -->
+    <!--  Mobile Viewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Use Iconifyer to generate all the favicons and touch icons you need: http://iconifier.net -->
-    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="shortcut icon" href="<?php bloginfo( 'template_url' ); ?>/assets/img/favicon.png" />
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" />
 
     <!-- To avoid any format picker app -->
     <meta name="format-detection" content="telephone=no">
@@ -42,6 +37,7 @@
     <?php wp_head(); ?>
 
   </head>
+  
   <body <?php body_class(); ?>>
 
     <noscript>
